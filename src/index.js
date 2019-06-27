44eab9b3b8f3b1d1389a102a731397f5ce2ee8ab
Cory Hall <corydantehall@gmail.com>
2019-06-27 19:37:32 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+!(()=>{
+  const attatchSubmenuTrigger = (triggerElement) => {
+    const {parentElement} = triggerElement;
+    const {body} = document;
+    const bgElement = document.createElement('div')
+    const openFunction = (e) => {
+      e.preventDefault();
+      parentElement.classList.add('open')
+      body.classList.add('submenu-open')
+      triggerElement.removeEventListener('click', openFunction)
+      triggerElement.addEventListener('click', closeFunction)
+    };
+    const closeFunction = (e) => {
+      e.preventDefault();
+      parentElement.classList.remove('open')
+      body.classList.remove('submenu-open')
+      triggerElement.removeEventListener('click', closeFunction)
+      triggerElement.addEventListener('click', openFunction)
+    }
+    bgElement.classList.add('submenu-background-cover')
+    body.appendChild(bgElement)
+    bgElement.addEventListener('click', closeFunction)
+    triggerElement.addEventListener('click', openFunction)
+  }
+
+  document.querySelectorAll('.nav-menu-trigger').forEach(attatchSubmenuTrigger)
+})()
